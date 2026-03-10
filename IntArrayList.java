b5a3b5777e7d70c7210981152f23e84429cfb7dd
Rohith <rohith.g.ranga@gmail.com>
2026-03-09 22:12:14 -0400

    new features to arraylist class

--- IntArrayList.java
+++ IntArrayList.java
@@ -7,9 +7,17 @@ public class IntArrayList {
         data = new int[10];
         size = 0;
     }
+
     public IntArrayList(int capacity) {//initializing the array for any possible capacity
         data = new int[capacity];
         size = 0;
+    }
+
+    private boolean isValidIndex(int index) {
+        if (index < size && index >= 0) {
+            return true;
+        }
+        return false;
     }
 
     private void reallocate() {
@@ -21,13 +29,13 @@ public class IntArrayList {
     }
 
     private void shiftLeft(int position) {
-    for (int i = position; i < size - 1; i++) {
-        data[i] = data[i + 1];
+        for (int i = position; i < size - 1; i++) {
+            data[i] = data[i + 1];
+        }
+        size--;
     }
-    size--;
-}
 
-private void shiftRight(int position) {
+    private void shiftRight(int position) {
         if (size == data.length) {
             reallocate();
         }
@@ -59,7 +67,7 @@ private void shiftRight(int position) {
         size++;
     }
 
-    public int remove(int index) {
+    public int removeAt(int index) {
         int removed = data[index];
         for (int i = index; i < size - 1; i++) {
             data[i] = data[i + 1];
@@ -67,5 +75,46 @@ private void shiftRight(int position) {
         size--;
         return removed;
     }
-    
+
+    public boolean removeVal(int val) {
+        for (int i : data) {
+            if (i == val) {
+                removeAt(i);
+                return true;
+            }
+        }
+        return false;
     }
+    
+    @Override
+    public String toString() {
+        if (isEmpty()) return "empty";
+        String str = "";
+        for (int i : data) {
+            str += i + ", ";
+        }
+        return str;
+    }
+
+    public boolean isEmpty() {
+        if (size == 0) return true;
+        return false;
+    }
+
+    public int get(int index) {
+        if (isValidIndex(index)) {
+            return data[index];
+        } else {
+            throw new ArrayIndexOutOfBoundsException("The index " +  index + " is out of bounds.");
+        }
+    }
+    
+    public void set(int index, int val) {
+        if (isValidIndex(index)) {
+            data[index] = val;
+        } else {
+            throw new ArrayIndexOutOfBoundsException("The index " +  index + " is out of bounds.");
+        }
+    }
+    
+}
